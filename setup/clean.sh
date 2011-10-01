3c00e54c9ed46b7b2a7ed1c5c8082231c8f55d39
AlienMind <alienmind@gmail.com>
2011-10-01 08:20:40 +0200

    clean script from alienmod

---
+++ setup/clean.sh
@@ -0,0 +1,24 @@
+#!/system/bin/sh
+
+# Remove older version of my data apps
+cd /data/app/
+rm -f \
+      RILInfos.apk \
+      CallRecorder*.apk \
+      LordModUV*.apk
+      dev.sci.systune-*.apk \
+      com.google.android.gm-*.apk \
+      com.android.vending*.apk \
+      com.adobe.flashplayer-*.apk \
+      com.bigtincan.android.adfree-*.apk \
+      com.keramidas.TitaniumBackup-*.apk \
+      com.martino2k6.fontchangerlite-*.apk \
+      com.xtralogic.android.logcollector-*.apk \
+      com.google.android.apps.authenticator-*.apk \
+      com.google.android.apps.maps-*.apk \
+      com.google.android.apps.reader-*.apk \
+      com.google.android.street-*.apk \
+      com.google.android.youtube-*.apk \
+      com.antivirus-*.apk \
+      org.alienmod*.apk \
+cd -
