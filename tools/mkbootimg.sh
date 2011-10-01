c2ff55d5b743ea99854f5f68a3ddb966f435519b
AlienMind <alienmind@gmail.com>
2011-10-01 23:17:31 +0200

    Working local conversion zImage => boot.img

--- tools/mkbootimg.sh
+++ tools/mkbootimg.sh
@@ -1,6 +1,16 @@
-KERNELDIR=$1
-BOOT=$2
-dd if=$KERNELDIR/zImage of=$BOOT.tmp
-unpackbootimg $BOOT.tmp $KERNELDIR
-mkbootimg --kernel $KERNELDIR/zImage --ramdisk $KERNELDIR/boot.img-ramdisk.gz --cmdline `cat $KERNELDIR/boot.img-cmdline` \
-                            --base `cat $KERNELDIR/boot.img-base` --output $2
+#!/bin/bash
+export PATH=$PATH:tools
+if [ "$#" != "3" ]; then
+  echo "Usage: $0 <original_boot.img> <kernel_zImage> <output_boot.img>"
+  exit 1
+fi
+T=$PWD/.mkbootimgtmp
+mkdir -p $T
+ORIBOOTIMG=$1
+ZIMAGE=$2
+BOOT=$3
+dd if=$ORIBOOTIMG of=$BOOT.tmp
+unpackbootimg -i $BOOT.tmp -o $T
+mkbootimg --kernel $ZIMAGE --ramdisk $T/boot.img.tmp-ramdisk.gz --cmdline `cat $T/boot.img.tmp-cmdline` \
+                            --base `cat $T/boot.img.tmp-base` --output $BOOT
+rm -rf $BOOT.tmp $T
