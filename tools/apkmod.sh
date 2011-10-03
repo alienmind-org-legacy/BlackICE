f81e87dda1f5da8d7b9d862555f909f6000d7b50
AlienMind <alienmind@gmail.com>
2011-10-03 18:22:01 +0200

    apkmod now uses apktool and needs android_root (aapt)

--- tools/apkmod.sh
+++ tools/apkmod.sh
@@ -13,13 +13,16 @@ OUT_DIR=work/`basename $APK .apk`
 OUT_DIR=`FixPath $OUT_DIR`
 NEWAPK=work/`basename $APK`
 NEWAPK=`FixPath $NEWAPK`
+APKTOOL=$PWD/tools/apktool.jar 
 
 if [ -f "$APK" ]; then
-  mkdir -p $OUT_DIR
-  cd $OUT_DIR
-  unzip -ox $APK &> /dev/null
+#  mkdir -p $OUT_DIR
+#  cd $OUT_DIR
+  #unzip -ox $APK &> /dev/null
+  java -jar $APKTOOL d $APK $OUT_DIR
   cp -a $MOD_DIR/* .
-  zip -r9 $NEWAPK . &>/dev/null
+  #zip -r9 $NEWAPK . &>/dev/null
+  java -jar $APKTOOL b $OUT_DIR $NEWAPK
   #mv $APK $APK.orig # if we want to preserve
   sign.sh $NEWAPK $APK
   cd - &> /dev/null
