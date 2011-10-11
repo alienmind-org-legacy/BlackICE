2bdb8f531ec12a2eaae35d6d214673daa3cb50a0
AlienMind <alienmind@gmail.com>
2011-10-11 09:03:51 +0200

    Fixed abs path preventing to build the image

--- tools/mkbootimg.sh
+++ tools/mkbootimg.sh
@@ -9,8 +9,9 @@ mkdir -p $T
 ORIBOOTIMG=$1
 ZIMAGE=$2
 BOOT=$3
-dd if=$ORIBOOTIMG of=$BOOT.tmp
-unpackbootimg -i $BOOT.tmp -o $T
-mkbootimg --kernel $ZIMAGE --ramdisk $T/$BOOT.tmp-ramdisk.gz --cmdline `cat $T/$BOOT.tmp-cmdline` \
-                            --base `cat $T/$BOOT.tmp-base` --output $BOOT
-rm -rf $BOOT.tmp $T
+TMPBOOT=`basename $3`
+dd if=$ORIBOOTIMG of=$TMPBOOT.tmp
+unpackbootimg -i $TMPBOOT.tmp -o $T
+mkbootimg --kernel $ZIMAGE --ramdisk $T/$TMPBOOT.tmp-ramdisk.gz --cmdline `cat $T/$TMPBOOT.tmp-cmdline` \
+                            --base `cat $T/$TMPBOOT.tmp-base` --output $BOOT
+rm -rf $TMPBOOT.tmp $T
