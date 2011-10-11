04d90728e6cd764ff78337c0b765679d0882804f
AlienMind <alienmind@gmail.com>
2011-10-11 06:24:44 +0200

    Fix situation that mkbootimg.sh uses a diferent name for output

--- tools/mkbootimg.sh
+++ tools/mkbootimg.sh
@@ -11,6 +11,6 @@ ZIMAGE=$2
 BOOT=$3
 dd if=$ORIBOOTIMG of=$BOOT.tmp
 unpackbootimg -i $BOOT.tmp -o $T
-mkbootimg --kernel $ZIMAGE --ramdisk $T/boot.img.tmp-ramdisk.gz --cmdline `cat $T/boot.img.tmp-cmdline` \
-                            --base `cat $T/boot.img.tmp-base` --output $BOOT
+mkbootimg --kernel $ZIMAGE --ramdisk $T/$BOOT.tmp-ramdisk.gz --cmdline `cat $T/$BOOT.tmp-cmdline` \
+                            --base `cat $T/$BOOT.tmp-base` --output $BOOT
 rm -rf $BOOT.tmp $T
