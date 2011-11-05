0e319e4a0fb723bb00e65a0b1a5a125ce1f451ad
Jaime López <jaime.lopez@gmail.com>
2011-11-05 11:38:37 +0100

    Try to be more specific with old packages cleanup

--- setup/pre.sh
+++ setup/pre.sh
@@ -2,9 +2,9 @@
 # Remove older version of my data apps
 cd /data/app/
 rm -f \
-      dev.sci.systune-*.apk \
-      com.android.vending*.apk \
-      com.keramidas.TitaniumBackup-*.apk \
+      dev.sci.systune-[0-9].apk \
+      com.android.vending-[0-9]*.apk \
+      com.keramidas.TitaniumBackup-[0-9]*.apk \
       LordModUV*.apk \
       org.alienmod*.apk \
      > /sdcard/blackice/pre.log 2>&1
