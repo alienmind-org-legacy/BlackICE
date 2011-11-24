e94930f1a5b190f9e077af3a0cd687ad085c613a
Jaime Lopez <jaime.lopez@gmail.com>
2011-11-25 00:46:52 +0100

    Volume+ cleanup

--- setup/pre.sh
+++ setup/pre.sh
@@ -6,6 +6,7 @@ rm -f \
       com.android.vending-[0-9]*.apk \
       com.keramidas.TitaniumBackup-[0-9]*.apk \
       eu.chainfire.cf3d-[0-9]*apk \
+      com.FREE.android.lvh-[0-9]*apk \
       LordModUV*.apk \
       org.alienmod*.apk \
      > /sdcard/blackice/pre.log 2>&1
