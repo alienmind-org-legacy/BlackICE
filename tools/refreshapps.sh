12080b1a56744feef31179f9d7999fded9985ea6
Brian Larsen <brian@chezbel.com>
2012-03-25 16:22:12 +0800

    Reworked refreshapps.sh, changed wget to use -nv (not verbose)
    refreshapps.sh now pulls all matching .apk files from the phone into a
    temp directory and then does a diff against the existing ones in
    the download directory. The temp directory is left with only the
    .apk files that are different and which should be uploaded to
    buildpackages on the server.
    
    Renamed the orginal refreshapps.sh to refreshapps_old.sh in case
    it is still needed.

--- tools/refreshapps.sh
+++ tools/refreshapps.sh
@@ -1,13 +1,46 @@
+REFRESH_DOWNLOAD_DIR=refresh_download
+
+rm -rf ${REFRESH_DOWNLOAD_DIR}
+mkdir ${REFRESH_DOWNLOAD_DIR}
+
+echo ""
+echo "Extracting files from phone..."
+echo ""
 for i in *.apk; do
-  N=`echo $i | sed 's/-[0-9]*.apk//g' | sed 's/.apk//g'` 
+  N=`echo $i | sed 's/-[0-9]*.apk//g' | sed 's/.apk//g'`
   LOCAL=`ls $N*.apk | tr '\n' ' ' | tr '\r' ' '`
   REMOTO=`adb shell basename /data/app/$N*.apk | tr '\n' ' ' | tr '\r' ' ' | sed 's/\ $//g'`
-  if [ "$LOCAL" != "$REMOTO" -a "$REMOTO" != "$N*.apk " ]; then
-    echo "#LOCAL: $LOCAL" 
-    echo "#REMOTO: $REMOTO|" 
-    echo "#N: $N*.apk|" 
-    echo "#SYNC: "
-    echo " rm -f $LOCAL ; adb pull /data/app/$REMOTO ; mv $REMOTO $LOCAL"
-    echo "#--------------------"
+
+  TEST_FILE=`adb shell ls /data/app/${REMOTO} | grep -o "No such file"`
+  if [ "${TEST_FILE}" != "No such file" ]; then
+    # Pull all the matching .apks into the download dir
+    echo "adb pull /data/app/${REMOTO} ${REFRESH_DOWNLOAD_DIR}/${LOCAL}"
+    adb pull /data/app/${REMOTO} ${REFRESH_DOWNLOAD_DIR}/${LOCAL}
   fi
 done
+
+echo ""
+echo "Comparing files for differences..."
+echo "  - New extracted files will be copied"
+echo "  - Identical extracted files will be deleted"
+echo ""
+
+for i in ${REFRESH_DOWNLOAD_DIR}/*.apk; do
+  oldi=`basename ${i}`
+
+  echo "diff -s ${i} ${oldi}"
+#  diff -s ${i} ${oldi}
+  DIFF_FILE=`diff -s ${i} ${oldi} | grep -o "identical"`
+  if [ "${DIFF_FILE}" == "identical" ]; then
+    echo "  identical: rm ${i}"
+    rm ${i}
+  else
+    echo "  different: cp ${i} ${oldi}"
+    cp ${i} ${oldi}
+  fi
+done
+
+echo ""
+echo "  New files that should be uploaded to server"
+echo ""
+ls -al ${REFRESH_DOWNLOAD_DIR}
---
+++ tools/refreshapps_old.sh
@@ -0,0 +1,18 @@
+REFRESH_DIR=temprefresh
+rm -rf ${REFRESH_DIR}
+mkdir ${REFRESH_DIR}
+
+for i in *.apk; do
+  N=`echo $i | sed 's/-[0-9]*.apk//g' | sed 's/.apk//g'`
+  LOCAL=`ls $N*.apk | tr '\n' ' ' | tr '\r' ' '`
+  REMOTO=`adb shell basename /data/app/$N*.apk | tr '\n' ' ' | tr '\r' ' ' | sed 's/\ $//g'`
+  if [ "$LOCAL" != "$REMOTO" -a "$REMOTO" != "$N*.apk " ]; then
+    echo "#LOCAL: $LOCAL"
+    echo "#REMOTO: $REMOTO|"
+    echo "#N: $N*.apk|"
+    echo "#SYNC: "
+    echo " rm -f $LOCAL ; adb pull /data/app/$REMOTO ; mv $REMOTO $LOCAL"
+    echo "#--------------------"
+    adb pull /data/app/$REMOTO ${REFRESH_DIR}/${LOCAL}
+  fi
+done
