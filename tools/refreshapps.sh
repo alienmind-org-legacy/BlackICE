4e1730d8904f1f13610f6a242ab80f99a729bd47
Brian Larsen <brian@chezbel.com>
2012-04-05 01:46:04 +0800

    Added the latest vending.apk (3.5.16) and improvements to refreshapps.sh

--- tools/refreshapps.sh
+++ tools/refreshapps.sh
@@ -1,12 +1,15 @@
 REFRESH_DOWNLOAD_DIR=refresh_download
-
 rm -rf ${REFRESH_DOWNLOAD_DIR}
 mkdir ${REFRESH_DOWNLOAD_DIR}
 
 echo ""
-echo "Extracting files from phone..."
-echo ""
+DOTS="Extracting matching *.apk files from phone "
 for i in *.apk; do
+  # -n = no newline
+  # -e = interpret escape characters
+  # \r = carriage return, but no newline
+  DOTS="${DOTS}."
+  echo -n -e "\r${DOTS}"
   N=`echo $i | sed 's/-[0-9]*.apk//g' | sed 's/.apk//g'`
   LOCAL=`ls $N*.apk | tr '\n' ' ' | tr '\r' ' '`
   REMOTO=`adb shell basename /data/app/$N*.apk | tr '\n' ' ' | tr '\r' ' ' | sed 's/\ $//g'`
@@ -14,33 +17,30 @@ for i in *.apk; do
   TEST_FILE=`adb shell ls /data/app/${REMOTO} | grep -o "No such file"`
   if [ "${TEST_FILE}" != "No such file" ]; then
     # Pull all the matching .apks into the download dir
-    echo "adb pull /data/app/${REMOTO} ${REFRESH_DOWNLOAD_DIR}/${LOCAL}"
-    adb pull /data/app/${REMOTO} ${REFRESH_DOWNLOAD_DIR}/${LOCAL}
+##    echo "adb pull /data/app/${REMOTO} ${REFRESH_DOWNLOAD_DIR}/${LOCAL}"
+    adb pull /data/app/${REMOTO} ${REFRESH_DOWNLOAD_DIR}/${LOCAL} &>/dev/null
   fi
 done
-
-echo ""
-echo "Comparing files for differences..."
-echo "  - New extracted files will be copied"
-echo "  - Identical extracted files will be deleted"
 echo ""
 
+NEW_FILES=0
 for i in ${REFRESH_DOWNLOAD_DIR}/*.apk; do
   oldi=`basename ${i}`
-
-  echo "diff -s ${i} ${oldi}"
-#  diff -s ${i} ${oldi}
   DIFF_FILE=`diff -s ${i} ${oldi} | grep -o "identical"`
   if [ "${DIFF_FILE}" == "identical" ]; then
-    echo "  identical: rm ${i}"
     rm ${i}
   else
-    echo "  different: cp ${i} ${oldi}"
-    cp ${i} ${oldi}
+    NEW_FILES=1
   fi
 done
 
 echo ""
-echo "  New files that should be uploaded to server"
+if [ "$NEW_FILES" == "1" ]; then
+  echo "The following are different and should be uploaded to the server"
+  for i in ${REFRESH_DOWNLOAD_DIR}/*.apk; do
+    echo "  ${i}"
+  done
+else
+  echo "  No new files"
+fi
 echo ""
-ls -al ${REFRESH_DOWNLOAD_DIR}
