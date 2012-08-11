f147827402bd48650343485216f4a5299e3bf0df
Brian Larsen <brian@chezbel.com>
2012-08-11 11:55:33 +0800

    Updated for nightly build. Added gapps 3.7.15, updated some Apps.

--- setup/post.sh
+++ setup/post.sh
@@ -2,7 +2,7 @@
 
 # Install default market
 if [ ! -f /system/app/Vending.apk ]; then
-  cp -p /sdcard/blackice/market/Vending-3.7.13.apk /system/app/Vending.apk
+  cp -p /sdcard/blackice/market/Vending-3.7.15.apk /system/app/Vending.apk
   chmod 644 /system/app/Vending.apk
 fi
 
